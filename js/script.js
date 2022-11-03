d2f42c6af5c40f1c3c153e1b61c04c68651dca77
taufik69 <taufik35-2915@diu.edu.bd>
2022-11-04 02:08:33 +0600

    2nd modification

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@
 let box = document.querySelector('.box');
 let btn = document.querySelector('.btn');
 
-// btn.addEventListener('mouseover',()=>{
+btn.addEventListener('mouseover',()=>{
     
-//     box.classList.toggle('change_color');
-//     box.innerHTML = 'motherchod';
-//     box.style.color = 'white';
-//     box.style.fontSize = '100px';
-//     box.style.textAlign = 'center';
+    box.classList.toggle('change_color');
+    box.innerHTML = 'motherchod';
+    box.style.color = 'white';
+    box.style.fontSize = '100px';
+    box.style.textAlign = 'center';
 
-// })
+})
 
 
 let div = document.createElement('div');
@@ -28,6 +28,8 @@ console.log(div);
 let container = document.querySelector('.container');
 // container.insertBefore(div, btn);
 container.append(div);
+
+// my name is taufik
 
 
 
