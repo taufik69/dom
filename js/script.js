f7aed35f75da7ab469fe89665a073633c1d454e0
taufik69 <taufik35-2915@diu.edu.bd>
2022-11-04 01:16:48 +0600

    js code

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+// let parent = document.querySelector('ul');
+// console.log(parent)
+
+//  let children = parent.children;
+//  console.log(children);
+
+let box = document.querySelector('.box');
+let btn = document.querySelector('.btn');
+
+// btn.addEventListener('mouseover',()=>{
+    
+//     box.classList.toggle('change_color');
+//     box.innerHTML = 'motherchod';
+//     box.style.color = 'white';
+//     box.style.fontSize = '100px';
+//     box.style.textAlign = 'center';
+
+// })
+
+
+let div = document.createElement('div');
+div.className = 'bad_boy'
+div.setAttribute('id', 'red');
+
+console.log(div);
+
+let container = document.querySelector('.container');
+// container.insertBefore(div, btn);
+container.append(div);
+
+
+
+
+
